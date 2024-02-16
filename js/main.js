b2f7852dffb907c51c9d72bee6c4aeda0842bb9a
Pol <polmonteroalegre@gmail.com>
2024-02-16 19:17:21 +0100

    FEAT basic movement done smoothly

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 console.log("THREE INIT FINE ON MAIN.JS...")
-let targetRotationX = 0.05
-let targetRotationY = 0.02
-let mouseX, mouseXOnMouseDown, mouseY, mouseYOnMouseDown = 0 //lol
-const windowHalfX = window.innerWidth / 2
-const windowHalfY = window.innerHeight / 2
-const dragFactor = 0.0002
+let targetRotationX = 0.05;
+let targetRotationY = 0.02;
+let mouseX = 0, mouseXOnMouseDown = 0, mouseY = 0, mouseYOnMouseDown = 0;
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+const slowingFactor = 0.90;
+const dragFactor = 0.0002;
 
-const onDocumentMouseMove = (event) => {
-    mouseX = event.clientX - windowHalfX
-    mouseY = event.clientY - windowHalfY
-    targetRotationX = (mouseX - mouseXOnMouseDown) * dragFactor
-    targetRotationY = (mouseY - mouseYOnMouseDown) * dragFactor
+function onDocumentMouseDown(event) {
+    event.preventDefault();
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    mouseXOnMouseDown = event.clientX - windowHalfX;
+    mouseYOnMouseDown = event.clientY - windowHalfY;
 }
 
-const onDocumentMouseDown = (event) => {
-    event.preventDefault()
-    document.addEventListener("mousemove", onDocumentMouseMove, false);
-    mouseXOnMouseDown = event.clientX - windowHalfX
-    mouseYOnMouseDown = event.clientY - windowHalfY
+function onDocumentMouseMove(event) {
+    mouseX = event.clientX - windowHalfX;
+    targetRotationX = (mouseX - mouseXOnMouseDown) * dragFactor;
+    mouseY = event.clientY - windowHalfY;
+    targetRotationY = (mouseY - mouseYOnMouseDown) * dragFactor;
+}
+
+function onDocumentMouseUp(event) {
+    document.removeEventListener('mousemove', onDocumentMouseMove, false);
+    document.removeEventListener('mouseup', onDocumentMouseUp, false);
 }
 
 const main = () => {
@@ -57,9 +64,12 @@ const main = () => {
     // camera.position.y = 2
     camera.position.z = 2
 
-    //thats self explanatory...
+    //FOR EACH ANIMATION FRAME RENDERS ->
     const render = () => {
-        earthMesh.rotateOnAxis(new THREE.Vector3(0, 0.5, 0.08), 0.001)
+        earthMesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), targetRotationX)
+        earthMesh.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), targetRotationY)
+        targetRotationY *= slowingFactor
+        targetRotationX *= slowingFactor
         renderer.render(scene, camera)
     }
 
